3f0909fc8662dcdc2b8c4b6bbbf7118798fa9651
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-04-18 19:04:03 +0900

    initialize

---
+++ Droid/ExWebViewRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Webkit;
+using Xamarin.Forms.CallingJavascriptSample;
+using Xamarin.Forms.CallingJavascriptSample.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using WebView = Xamarin.Forms.WebView;
+
+[assembly: ExportRenderer(typeof(ExWebView), typeof(ExWebViewRenderer))]
+namespace Xamarin.Forms.CallingJavascriptSample.Droid
+{
+	class ExWebViewRenderer:WebViewRenderer{
+
+
+		protected override void OnElementChanged(ElementChangedEventArgs<WebView> e){
+			base.OnElementChanged(e);
+
+			var exWebView = e.NewElement as ExWebView;
+			if (exWebView != null) {
+				exWebView.CallJs = CallJs;
+			}
+			Control.SetWebChromeClient(new WebChromeClient());
+		}
+		public void CallJs(string js){
+			//Android.Webkit.WebView
+			Control.LoadUrl("javascript:" + js);
+		}
+
+	}
+}
---
+++ iOS/ExWebViewRenderer.cs
@@ -0,0 +1,28 @@
+﻿using CallingJavascriptSample;
+using CallingJavascriptSample.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms.CallingJavascriptSample;
+
+[assembly: ExportRenderer(typeof(ExWebView), typeof(ExWebViewRenderer))]
+namespace CallingJavascriptSample.iOS
+{
+	public class ExWebViewRenderer : WebViewRenderer
+	{
+		protected override void OnElementChanged(VisualElementChangedEventArgs e) {
+			base.OnElementChanged(e);
+
+			var exWebView = e.NewElement as ExWebView;
+			if (exWebView!=null) {
+				exWebView.CallJs = CallJs;
+			}
+		}
+
+		public void CallJs(string js) {
+			//this = UIKit.UIWebView
+			//this.EvaluateJavascript(js);
+			EvaluateJavascript(js);
+		}
+
+	}
+}
